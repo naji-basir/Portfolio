47459563a5623b35597504211ef0e2d10953a696
Basir Naji <basirnaji007@gmail.com>
2026-04-12 16:34:48 +0430

    contact me

---
+++ src/components/ContactMe.jsx
@@ -0,0 +1,9 @@
+function ContactMe() {
+  return (
+    <div className="bg-indigo-200 dark:bg-gray-700 rounded-xl py-20 text-center">
+      <h1>ContactMe</h1>
+    </div>
+  );
+}
+
+export default ContactMe;
